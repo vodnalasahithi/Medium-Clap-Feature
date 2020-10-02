b0299b94ffb47756c6ed9302d66ebc4cece03506
Vodnala Sahithi <sahithivodnala66@gmail.com>
2020-10-02 22:03:49 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Usage from "./patterns/09";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Usage />
+      </header>
+    </div>
+  );
+}
+
+export default App;
